e9fb12302d973d5d85aafe80c1257a0e8ef59ae1
Banderolka <bothisava@gmail.com>
2018-06-12 22:55:16 +0400

    Изменина логика работы класса Calculator. Добавлено три класса арифметичеких действий

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,20 +1,47 @@
 import java.util.Scanner;
+
+public class Division{
+
+}
+
+public class Subtraction{
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Введите уменьшаемое");
+    double a = scanner.nextDouble();
+    System.out.println("Ведите вычитаемое");
+    double b = scanner.nextDouble();
+    double c = a-b;
+    System.out.print("Разность равна ");
+    System.out.printf("%.4f",c);
+    scanner.close();
+        }
+
+public class Multiplication{
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Введите первый множитель");
+    double a = scanner.nextDouble();
+    System.out.println("Ведите второе множитель");
+    double b = scanner.nextDouble();
+    double c = a*b;
+    System.out.print("Произведение равно ");
+    System.out.printf("%.4f",c);
+    scanner.close();
+}
 /**
  * @see Class Calculator выполняет сложение дробных чисел
  * @author Гудименко Олег
 */
 public class Calculator {
-    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите первое число");
+        System.out.println("Введите первое слагаемое");
         double a = scanner.nextDouble();
-        System.out.println("Ведите второе чилсо");
+        System.out.println("Ведите второе слагаемое");
         double b = scanner.nextDouble();
         double c = a+b;
         System.out.print("Сумма равна ");
         System.out.printf("%.4f",c);
         scanner.close();
     }
-}
+
 
 
